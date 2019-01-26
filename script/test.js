69bfd30cb4e9ef631e7ded4eacb5426018708384
alexmedve <41807747+alexmedve@users.noreply.github.com>
2019-01-26 21:15:22 +0200

    Delete test.js

--- script/test.js
+++
@@ -1,9 +0,0 @@
- let xhr = new XMLHttpRequest();
- xhr.open("GET", "http://randomword.setgetgo.com/");
- xhr.onload = function () {
-     if (this.status == 200 && word != "") {
-         console.log(2)
-     }
- }
- xhr.send();
- console.log(xhr);
